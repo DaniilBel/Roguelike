dd88bb4a8dcc2f74a08ea1785b856ed674fcf4b0
dan <danya.belyaev@gmail.com>
2024-01-07 10:40:23 +0300

    Add contusion

---
+++ src/main/java/org/example/entity/PersonAttack.java
@@ -0,0 +1,32 @@
+package org.example.entity;
+
+import java.util.Random;
+
+public class PersonAttack extends Person {
+    Person person;
+    Random random;
+    public PersonAttack(Person person) {
+        super(person.getX(), person.getY(), person.getHitPoints());
+        this.person = person;
+        random = new Random();
+    }
+
+    public void attack(Monster monster) {
+
+        if (monster.getDefense() - person.getStrength() >= 0)
+            monster.setHitPoints(-1);
+        else
+            monster.setHitPoints(-person.getStrength() + monster.getDefense());
+
+        switch(random.nextInt(4)) {
+            case 0:
+                monster.setPos(monster.getX()+10, monster.getY());
+            case 1:
+                monster.setPos(monster.getX()-10, monster.getY());
+            case 2:
+                monster.setPos(monster.getX(), monster.getY()+10);
+            case 3:
+                monster.setPos(monster.getX(), monster.getY()-10);
+        }
+    }
+}
--- src/test/java/org/example/engine/gui/GameWindowTest.java
+++ src/test/java/org/example/engine/gui/GameWindowTest.java
@@ -1,4 +1,4 @@
-package org.example.gui;
+package org.example.engine.gui;
 
 import org.junit.jupiter.api.Test;
 
